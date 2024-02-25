090d64eaa96e69d0f2cd71193f3a9e06d2877dea
dcbrown <browncdonovan@gmail.com>
2024-02-25 15:04:05 -0500

    finished section 2

--- app/page.js
+++ app/page.js
@@ -1,5 +1,41 @@
-const HomePage = () => {
-  return <div>HomePage</div>;
-};
+import Hero from "../components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
+import Footer from "@/components/Footer";
+import properties from "@/properties.json";
+import PropertyCard from "@/components/PropertyCard";
+import Link from "next/link";
 
+const HomePage = () => {
+  const recentProperties = properties
+    .sort(() => Math.random() - Math.random())
+    .slice(0, 3);
+
+  return (
+    <>
+      <Hero />
+      <InfoBoxes />
+      <section class="px-4 py-6">
+        <div class="container-xl lg:container m-auto">
+          <h2 class="text-3xl font-bold text-blue-500 mb-6 text-center">
+            Recent Properties
+          </h2>
+          <div class="grid grid-cols-1 md:grid-cols-3 gap-6">
+            {recentProperties.map((property, index) => (
+              <PropertyCard property={property} key={index} />
+            ))}
+          </div>
+        </div>
+      </section>
+      <section className="m-auto max-w-lg my-10 px-6">
+        <Link
+          href="/properties"
+          className="block bg-black text-white text-center py-4 px-6 rounded-xl hover:bg-gray-700"
+        >
+          View All Properties
+        </Link>
+      </section>
+      <Footer />
+    </>
+  );
+};
 export default HomePage;
