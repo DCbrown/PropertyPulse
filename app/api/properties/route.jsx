e9d2fc4434f8dc2d5b53afdccb45168e1d1ef201
dcbrown <browncdonovan@gmail.com>
2024-02-26 12:18:26 -0500

    Database and API routes, video 5

---
+++ app/api/properties/route.jsx
@@ -0,0 +1,11 @@
+import connectDB from "@/config/database";
+
+export const GET = async (request) => {
+  try {
+    return new Response(JSON.stringify({ message: "Hello World" }), {
+      status: 200,
+    });
+  } catch (error) {
+    return new Response("Something went wrong", { status: 500 });
+  }
+};
---
+++ config/database.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+let connected = false;
+
+const connectDB = async () => {
+  mongoose.set("strictQuery", true);
+
+  // If the connection is already established, don't connect to the database again
+
+  if (connected) {
+    console.log("=> using existing database connection");
+    return;
+  }
+
+  // Connect to the database
+  try {
+    await mongoose.connect(process.env.MONGODB_URI);
+    connected = true;
+    console.log("=> new database connection");
+  } catch (error) {
+    console.error("Error connecting to database: ", error);
+  }
+};
+
+export default connectDB;
